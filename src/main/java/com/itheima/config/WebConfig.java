d769325a222b118fc30a0d069a12373304579014
特仑苏 <2077152374@qq.com>
2025-07-21 10:21:44 +0800

    拦截器功能修复

---
+++ src/main/java/com/itheima/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.itheima.config;
+
+import com.itheima.interceptor.Logininterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private Logininterceptor logininterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logininterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
